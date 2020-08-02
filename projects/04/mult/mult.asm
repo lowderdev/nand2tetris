0d4d0f77f2b48b6c9b0a2940a120fe7300349bc0
Logan Lowder <loganlowder@gmail.com>
2020-08-01 20:44:51 -0500

    Implement multiplication in asm

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,50 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// This implementation does not handle negative arguments
+
+// pseudo code
+// --------------------
+// m = RAM[0]
+// n = RAM[1]
+// R2 = 0 // result address used as acc
+//
+// LOOP
+// JUMP to @END if n == 0
+// sum = sum + m
+// n = n - 1
+//
+
+  @R0
+  D=M
+  @m
+  M=D // m = RAM[0]
+
+  @R1
+  D=M
+  @n
+  M=D // n = RAM[1]
+
+  @R2
+  M=0 // acc = 0
+
+(LOOP)
+  @n
+  D=M
+  @END
+  D;JEQ
+
+  @m
+  D=M
+  @R2
+  M=M+D // acc = acc + m
+
+  @n
+  M=M-1 // n = n - 1
+
+  @LOOP
+  0;JMP // goto LOOP
+
+(END)
+  @END
+  0;JMP
